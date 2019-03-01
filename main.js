5cdcc8a3f509623bf26422d0e8d472b1f01960ae
tijloz <homerl3@aston.ac.uk>
2019-03-01 17:15:40 +0000

    add spi test

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const SerialPort = require('serialport')
 const { app, BrowserWindow, ipcMain } = electron;
 const os = require('os');
 const osPlatform = os.platform();
+const spi = require('spi-device');
 
 const port = new SerialPort(osSwap(), {
     baudRate: 9200
@@ -21,26 +22,35 @@ function osSwap() {
     }
 }
 
-// function osSwap(){
-//     if (osPlatform === 'win32') {
-//         return 'com3';
-//     } else if (osPlatform === 'linux') {
-//         return '/dev/tty-usbserial1';
-//     }
-// }
+// The Nucleo is on bus 0 and it's device 0
+const Nucleo = spi.open(0, 0, (err) => {
+    // An SPI message is an array of one or more read+write transfers
+    const message = [{
+        sendBuffer: Buffer.from([0x01, 0xd0, 0x00]), // Sent to read channel 5
+        receiveBuffer: Buffer.alloc(3),              // Raw data read from channel 5
+        byteLength: 3,
+        speedHz: 20000 // Use a low bus speed to get a good reading from the TMP36
+    }];
 
+    if (err) throw err;
+
+    Nucleo.transfer(message, (err, message) => {
+        if (err) throw err;
+
+        // Convert raw value from sensor to celcius and log to console
+        const rawValue = ((message[0].receiveBuffer[1] & 0x03) << 8) +
+            message[0].receiveBuffer[2];
+        const voltage = rawValue
+        console.log(voltage);
+    });
+});
 
 // SET ENV
 process.env.NODE_ENV = 'production';
 
-
-
 // Listen for app to be ready
 app.on('ready', function () {
 
-
-
-    
     /*      Create the main window      */
     let mainWindow = new BrowserWindow({
         show: false,
